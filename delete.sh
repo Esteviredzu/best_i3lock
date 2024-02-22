b74723a4e7bc64e03f57b0c6c9b5f5c7ea9e3e6a
Esteviredzu <82750197+Esteviredzu@users.noreply.github.com>
2024-02-22 13:43:38 +0500

    Update delete.sh

--- delete.sh
+++ delete.sh
@@ -1,3 +1,5 @@
 rm /usr/share/pixmaps/lockoverlay.png
-userr=$(whoami | awk '{print $1}')  
-rm /home/$userr/.config/i3/launch_besti3.sh
+echo "Enter your os username here: "
+read userr
+rm /home/$userr/.config/i3/launch_besti3.sh && echo "DONE"
+
