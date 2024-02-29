1fce5c0a4306f32753b8386237a872dbf35c4a3c
Esteviredzu <82750197+Esteviredzu@users.noreply.github.com>
2024-02-29 18:21:21 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 cp lockoverlay.png /usr/share/pixmaps/
 echo "Enter your os username here: "
 read userr
-cp launch_besti3.sh /home/$userr/.config/i3/
+cp i3lock-setup.sh /home/$userr/.config/i3/
 
-chmod +x /home/$userr/.config/i3/launch_besti3.sh && echo "Done. You can run it "sh /home/$userr/.config/i3/launch_besti3.sh" or bind execution to mod + l or another keyboard shortcut. f.e. bindsym $mod+l exec --no-startup-id /home/YOUR_USERNAME/.config/i3/i3lock-setup.sh"
+chmod +x /home/$userr/.config/i3/i3lock-setup.sh && echo "Done. You can run it "sh /home/$userr/.config/i3/i3lock-setup.sh" or bind execution to mod + l or another keyboard shortcut. f.e. bindsym $mod+l exec --no-startup-id /home/YOUR_USERNAME/.config/i3/i3lock-setup.sh"
