aba9773b26db538545b9988bc1e03dd71edc580f
Esteviredzu <82750197+Esteviredzu@users.noreply.github.com>
2024-02-29 18:19:34 +0500

    Update and rename launch_besti3.sh to i3lock-setup.sh

--- i3lock-setup.sh
+++ i3lock-setup.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 # Esteviredzu (veltall fork)
-
 overlay=/usr/share/pixmaps/lockoverlay.png
-
+setxkbmap us
 scrot /tmp/currentworkspace.png
 convert /tmp/currentworkspace.png -blur 0x30 /tmp/currentworkspaceblur.png
 composite -gravity southeast $overlay /tmp/currentworkspaceblur.png /tmp/lockbackground.png
 i3lock -i /tmp/lockbackground.png
-
+setxkbmap us,ru
 rm /tmp/currentworkspace.png
