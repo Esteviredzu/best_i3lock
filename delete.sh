37da937019f5bf0c90684cb771836df315afab5d
Esteviredzu <82750197+Esteviredzu@users.noreply.github.com>
2024-02-29 18:21:39 +0500

    Update delete.sh

--- delete.sh
+++ delete.sh
@@ -1,5 +1,5 @@
 rm /usr/share/pixmaps/lockoverlay.png
 echo "Enter your os username here: "
 read userr
-rm /home/$userr/.config/i3/launch_besti3.sh && echo "DONE"
+rm /home/$userr/.config/i3/i3lock-setup.sh && echo "DONE"
 
